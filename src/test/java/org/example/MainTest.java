782cd56638425e5a90795db8f08b31089cc97450
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-02-26 15:12:49 +0200

    add parts of test to Selenide report a
    it replaces custom logging with the built-in Selenide report - it conviniently shows duration of every step.

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -8,6 +8,7 @@ import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.junit.TextReport;
+import com.codeborne.selenide.logevents.SelenideLog;
 import org.junit.Rule;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -15,6 +16,7 @@ import org.openqa.selenium.WebElement;
 
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.function.Supplier;
 
 import static com.codeborne.selenide.Condition.exactOwnText;
 import static com.codeborne.selenide.Condition.exist;
@@ -24,7 +26,9 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.element;
-import static java.lang.System.currentTimeMillis;
+import static com.codeborne.selenide.logevents.LogEvent.EventStatus.PASS;
+import static com.codeborne.selenide.logevents.SelenideLogger.beginStep;
+import static com.codeborne.selenide.logevents.SelenideLogger.commitStep;
 
 /**
  * Created with IntelliJ IDEA.
@@ -39,28 +43,38 @@ public class MainTest {
 
     @Test
     public void verifySubscriptionItemsForOrder() {
+        SelenideLog log = beginStep("MainTest", "verify subscription recurring price");
         Configuration.pageLoadTimeout = 50000;
         Selenide.open("http://127.0.0.1:8080/my_account4/My%20Account.html");
 
-        long start = currentTimeMillis();
-        LoggerUtils.logMethodTime("find all subscriptions");
-        ElementsCollection divSubscriptionItems = $$("div.subs-expands").snapshot();
+        ElementsCollection divSubscriptionItems = step("find all subscriptions", 
+          () -> $$("div.subs-expands").snapshot());
 
-        LoggerUtils.logMethodTime("find all subscriptions related to the order");
         //find the subscription items for an order in subscription list, basically it retrieve all subscription items and filter by order id.
-        ElementsCollection subscriptions = findSubscriptionItemsByOrderId(divSubscriptionItems, "O-0000017865");
-        LoggerUtils.logMethodTime("find the subscription related to the product");
-        //find the subscription for the product SIMATIEDL, it uses the method `ElementCollection.filter()` to find the target subscription.
-        SelenideElement target = this.findSubscriptionItem(subscriptions, "SIMATIEDL", 1);
-        LoggerUtils.logMethodTime("verify product name.");
-        target.find(By.cssSelector("span.cc_col_subscription_prod_name a")).shouldHave(exactOwnText("Industrial Edge Management License"));
-        LoggerUtils.logMethodTime("verify subscription id");
-        target.find(By.cssSelector("span.cc_goto_subscription_details")).shouldHave(matchText("^SB-.*"));
-        LoggerUtils.logMethodTime("verify subscription recurring price.");
-        target.find(By.cssSelector("span.cc_col_subscription_recurring_price")).shouldHave(ownText("USD $"));
+        ElementsCollection subscriptions = step("find all subscriptions related to the order", 
+          () -> findSubscriptionItemsByOrderId(divSubscriptionItems, "O-0000017865"));
 
-        long end = currentTimeMillis();
-        LoggerUtils.logMethodTime("finished to verify subscription recurring price in " + (end - start) + " ms.");
+        //find the subscription for the product SIMATIEDL, it uses the method `ElementCollection.filter()` to find the target subscription.
+        SelenideElement target = step("find the subscription related to the product",
+          () -> findSubscriptionItem(subscriptions, "SIMATIEDL", 1));
+        
+        step("verify product name.", () -> 
+            target.find(By.cssSelector("span.cc_col_subscription_prod_name a")).shouldHave(exactOwnText("Industrial Edge Management License")));
+
+        step("verify subscription id", () -> 
+            target.find(By.cssSelector("span.cc_goto_subscription_details")).shouldHave(matchText("^SB-.*")));
+
+        step("verify subscription recurring price.", () -> 
+            target.find(By.cssSelector("span.cc_col_subscription_recurring_price")).shouldHave(ownText("USD $")));
+
+        commitStep(log, PASS);
+    }
+
+    private <T> T step(String title, Supplier<T> block) {
+        SelenideLog log = beginStep("MainTest", title);
+        T result = block.get();
+        commitStep(log, PASS);
+        return result;
     }
 
     public ElementsCollection findSubscriptionItemsByOrderId(ElementsCollection divSubscriptionItems, String orderNumber) {
@@ -77,10 +91,7 @@ public class MainTest {
 
         // TODO We could introduce method like $.cached() or $.freeze() in Selenide
         SelenideElement target = $(rawTarget.getWrappedElement());
-
-        LoggerUtils.logMethodTime("the target subscription item is found in the list.");
         target.scrollIntoView(true).shouldBe(visible);
-        LoggerUtils.logMethodTime("the target subscription item is scrolled into view");
         return target;
     }
 
